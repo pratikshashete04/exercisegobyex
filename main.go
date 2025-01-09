6d587afa56689e05af9b4dd215e8a75261e92a86
pratikshashete04 <pratikshashete04@gmail.com>
2025-01-09 15:58:58 +0530

    practicing printing of values

--- main.go
+++ main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hii...")
+	fmt.Println(!true)
+	fmt.Println("one plus one:", 1+1)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println("Hi" + "there")
+	fmt.Println("7.0/3.0:", 7.0/3.0)
 }
